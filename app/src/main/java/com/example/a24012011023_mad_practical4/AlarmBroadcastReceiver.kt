d9121001d1c4c4521afc51908535b816aa3c1584
Sampreeti Fondekar <sampreeti1010@gmail.com>
2026-07-30 09:49:46 +0530

    Added Service and others

---
+++ app/src/main/java/com/example/a24012011023_mad_practical4/AlarmBroadcastReceiver.kt
@@ -0,0 +1,24 @@
+package com.example.a24012011023_mad_practical4
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class AlarmBroadcastReceiver : BroadcastReceiver() {
+    companion object{
+        val SERVICE_KEY="Service1"
+        val START_VAL="start"
+        val STOP_VAL="stop"
+    }
+
+    override fun onReceive(context: Context, intent: Intent) {
+        val str1 = intent.getStringExtra(SERVICE_KEY)
+        if(str1==START_VAL || str1==STOP_VAL){
+            val intentService= Intent(context, AlarmService::class.java)
+            if(str1==START_VAL)
+                context.startService(intentService)
+            else
+                context.stopService(intentService)
+        }
+    }
+}
--- app/src/main/java/com/example/a24012011023_mad_practical4/MainActivity.kt
+++ app/src/main/java/com/example/a24012011023_mad_practical4/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.example.a24012011023_mad_practical4
 
+import android.app.TimePickerDialog
+import android.icu.util.Calendar
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.button.MaterialButton
+import com.google.android.material.card.MaterialCardView
 
 class MainActivity : AppCompatActivity() {
+    lateinit var textAlarm: TextView
+    lateinit var cardSetAlarm: MaterialCardView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +24,28 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        textAlarm=findViewById<TextView>(R.id.textView4)
+        cardSetAlarm=findViewById(R.id.card_list)
+        cardSetAlarm.visibility= View.GONE
+        findViewById<MaterialButton>(R.id.btnSetAlarm).setOnClickListener {
+
+        }
+        findViewById<MaterialButton>(R.id.btnCancelAlarm).setOnClickListener {
+
+        }
+    }
+    private fun showTimedialog(){
+        val cldr: Calendar= Calendar.getInstance()
+        val h:Int = cldr.get(Calendar.HOUR_OF_DAY)
+        val m:Int=cldr.get(Calendar.MINUTE)
+        val picker= TimePickerDialog(
+            this,
+            {tp,sHour,sMinute->sendDialogDataToActivity(sHour,sMinute)},
+            h,m,false
+        )
+        picker.show()
+    }
+    private fun sendDialogDataToActivity(hour:Int,minute:Int){
+
     }
 }
